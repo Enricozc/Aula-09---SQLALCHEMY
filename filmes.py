f034468742ad62f307df50df0fe1a8fc0d05fe7b
Enricozc <enrico.candido08@gmail.com>
2026-03-16 16:49:43 -0300

    Criando listar, atualizar e deletar

--- filmes.py
+++ filmes.py
@@ -52,5 +52,59 @@ def cadastrar_filme():
             session.rollback()
             print(f"Erro ao cadastrar filme: {erro}")
  
-
+#Criar as funções listar, atualizar e deletar
 cadastrar_filme()
+def listar_filmes():
+    print(f"\n---Lista de Filmes---")
+    with Session() as session:
+        try:
+            filmes = session.query(Filme).all()
+            if filmes:
+                for filme in filmes:
+                    print(f"ID: {filme.id} | Título: {filme.titulo} | Gênero: {filme.genero} | Ano: {filme.ano_lancamento} | Nota: {filme.nota} | Disponível: {'Sim' if filme.disponivel else 'Não'}")
+            else:
+                print("Nenhum filme cadastrado.")
+        except Exception as erro:
+            print(f"Erro ao listar filmes: {erro}")
+            session.rollback()
+            
+
+def atualizar_filme():
+    print(f"\n---Atualização de Filme---")
+    id_filme = int(input("Digite o ID do filme que deseja atualizar: "))
+    with Session() as session:
+        try:
+            filme = session.query(Filme).filter_by(id=id_filme).first()
+            if filme:
+                novo_titulo = input(f"Digite o novo título do filme (atual: {filme.titulo}): ")
+                novo_genero = input(f"Digite o novo gênero do filme (atual: {filme.genero}): ")
+                novo_ano_lancamento = int(input(f"Digite o novo ano de lançamento do filme (atual: {filme.ano_lancamento}): "))
+                nova_nota = float(input(f"Digite a nova nota do filme (atual: {filme.nota}): "))
+                filme.titulo = novo_titulo
+                filme.genero = novo_genero
+                filme.ano_lancamento = novo_ano_lancamento
+                filme.nota = nova_nota
+                session.commit()
+                print("Filme atualizado com sucesso.")
+            else:
+                print("Filme não encontrado.")
+        except Exception as erro:
+            session.rollback()
+            print(f"Erro ao atualizar filme: {erro}")
+
+
+def deletar_filme():
+    print(f"\n---Deleção de Filme---")
+    id_filme = int(input("Digite o ID do filme que deseja deletar: "))
+    with Session() as session:
+        try:
+            filme = session.query(Filme).filter_by(id=id_filme).first()
+            if filme:
+                session.delete(filme)
+                session.commit()
+                print("Filme deletado com sucesso.")
+            else:
+                print("Filme não encontrado.")
+        except Exception as erro:
+            session.rollback()
+            print(f"Erro ao deletar filme: {erro}")
